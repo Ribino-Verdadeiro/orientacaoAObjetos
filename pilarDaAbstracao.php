1e53ca8ea4dfdeb1687290b784b7adb52871d962
El Ribino <josefaustoribino05@gmail.com>
2024-08-21 16:51:15 -0300

    Add files via upload

--- pilarDaAbstracao.php
+++ pilarDaAbstracao.php
@@ -15,12 +15,38 @@
     
 
 <?php 
-class funcionario{
-    public $nome='Ribino';
-    public $telefone='15 99731-7353';
-    public $num_filhos=0;
 
-    function resumirCardFunc () {
+class Funcionario{
+    public $nome=null;
+    public $telefone=null;
+    public $num_filhos=null;
+
+    function setNome($nome){ // aqui eu seto que o atributo nome vai receber o que está na variavel $nome
+        $this->nome = $nome;
+    }
+
+    function setTelefone($telefone){
+        $this->telefone = $telefone;
+    }
+
+    function setNumFilhos($num_filhos){
+        $this->numFilhos = $num_filhos;
+    }
+
+    function getNome(){ // aqui eu pego o que está no atributo nome e retorno ele pra algum lugar
+        return $this->nome;
+    }
+
+    function getTelefone(){
+        return $this->telefone;
+    }
+
+    function getNumFilhos(){
+        return $this->numFilhos;
+    }
+//-------------------------------------------------------------------------------------------------------------------------
+
+    function resumirCardFunc () { // aqui são funções que eu criei para retornar no echo la embaixo
         return "$this->nome e ele possui $this->num_filhos filhos";
     }
 
@@ -30,11 +56,12 @@ class funcionario{
 
 }
 
-$variavel = new funcionario();
-
-    $variavel->modificarNumFilhos(4) . '<br>';
-    echo $variavel->resumirCardFunc();
-
+// o $y é necessario para armazerar as coisas do GET e do SET
+$y= new Funcionario(); // aqui eu crio um novo funcionario
+$y->setNome('Garrafinha'); // e coloco que o nome dele será garrafinha
+$y->setNumFilhos(100); // e que a garrafinha tem 100 filhos
+echo $y->resumirCardFunc() . "<br>"; // depois aqui eu pego a função que eu criei la em cima
+echo $y->getNome() . ' e possui ' . $y->getNumFilhos() . ' filhos  '; // aqui a mesma coisa porem eu concatenei aqui mesmo e nao na função pois usei 2 atributos diferentes
 
 ?>
 
